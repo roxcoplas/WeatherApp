7f7ab386134784b224a2ed2bfec70ffe2f49ed89
roxcoplas <coplasrox@gmail.com>
2025-06-12 02:14:28 +0800

    Added navbar design

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,30 @@
 import { Routes } from '@angular/router';
+import { LandingComponent } from './landing/landing.component';
+import { HomeComponent } from './home/home.component';
+import { WeatherComponent } from './weather/weather.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'landing',
+        pathMatch: 'full'
+    },
+    {
+        path: 'landing',
+        component: LandingComponent,
+    },
+    {
+        path: 'navbar',
+        component: NavbarComponent,
+    },
+    {
+        path: 'home',
+        component: HomeComponent,
+    },
+    {
+        path: 'weather',
+        component: WeatherComponent,
+    },
+];
--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { Router } from '@angular/router';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-navbar',
@@ -7,5 +10,15 @@ import { Component } from '@angular/core';
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
+  private router = inject(Router);
+  showLogoutBtn = toSignal(
+    this.router.events.pipe(
+      map(() => ['/home', '/weather'].includes(this.router.url))
+    ),
+    { initialValue: false }
+  );
 
+  logout() {
+    this.router.navigate(['/landing']);
+  }
 }
