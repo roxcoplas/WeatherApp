ccf12e8e1f05863601cf6e2245881e9296bdc2bc
roxcoplas <coplasrox@gmail.com>
2025-06-12 10:52:02 +0800

    Added home screen design

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,22 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { ReactiveFormsModule, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+  cityControl = new FormControl('');
 
+  constructor( private router: Router) {}
+
+  viewWeather() {
+    const city = this.cityControl.value;
+    if (city) {
+      this.router.navigate(['/weather'], { queryParams: { city } });
+    }
+  }
 }
